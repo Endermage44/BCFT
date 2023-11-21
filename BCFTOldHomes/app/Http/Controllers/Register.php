5626ab79798da8d2d95b80b2af5703df28f8b207
Franklinn19 <117764814+Franklinn19@users.noreply.github.com>
2023-11-21 14:46:46 -0500

    Created Register page and functionality

--- BCFTOldHomes/app/Http/Controllers/Register.php
+++ BCFTOldHomes/app/Http/Controllers/Register.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class Register extends Controller
 {
-    //
+    public function showRegistrationForm()
+    {
+        // You can implement this method to show the registration form view
+        return view('register');
+    }
+
+    public function register(Request $request)
+    {
+        // Validate the request data
+        $validatedData = $request->validate([
+            'role' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:users',
+            'phone' => 'required',
+            'password' => 'required',
+            'dob' => 'required',
+            // Add validation rules for other fields as needed
+        ]);
+
+        // Additional validation for Patient role
+        if ($validatedData['role'] === 'Patient') {
+            $request->validate([
+                'family_code' => 'required',
+                'emergency_contact' => 'required',
+                'relation_to_emergency_contact' => 'required',
+                // Add validation rules for other patient fields
+            ]);
+        }
+
+        // Create a new User instance and save to the database
+        $user = User::create($validatedData);
+
+        // Optionally, you can log the user in after registration
+        // auth()->login($user);
+
+        // Redirect to a success page or provide a success message
+        return redirect()->route('success'); // Adjust the route accordingly
+    }
+    //  THIS CODE SEEMS LIKE IT CAN WORK , Commented it to be safe , thought it could be a great start
 }
