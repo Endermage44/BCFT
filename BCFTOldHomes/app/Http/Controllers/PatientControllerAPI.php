629b5538452d4c98b2e9b8de26dd9a37bdc2edca
Endermage44 <bad0529@icloud.com>
2023-11-30 12:16:07 -0500

    Added admin page and changed names of files

--- BCFTOldHomes/app/Http/Controllers/PatientControllerAPI.php
+++ BCFTOldHomes/app/Http/Controllers/PatientControllerAPI.php
@@ -28,26 +28,26 @@ class PatientControllerAPI extends Controller
      */
 
      //New changes added , this code right here will contain an array of the patient's associated groups. To find the group for the Additional Patients Info Page
-     public function show($patientId)
+     public function show(string $id)
      {
-         $patient = Patients::find($patientId);
+        //  $patient = Patients::find($patientId);
  
-         if (!$patient) {
-             return response()->json(['error' => 'Patient not found'], 404);
-         }
+        //  if (!$patient) {
+        //      return response()->json(['error' => 'Patient not found'], 404);
+        //  }
  
-         // Load the patient's groups using the defined relationship
-         $patientGroups = $patient->groups;
+        //  // Load the patient's groups using the defined relationship
+        //  $patientGroups = $patient->groups;
  
-         return response()->json([
-             'patientId' => $patient->patientID,
-             'emergencyContactID' => $patient->emergencyContactID,
-             'admissionDate' => $patient->admissionDate,
-             'emailID' => $patient->emailID,
-             'familyCode' => $patient->familyCode,
-             'roleID' => $patient->roleID,
-             'patientGroups' => $patientGroups, // Include patient groups in the response
-         ]);
+        //  return response()->json([
+        //      'patientId' => $patient->patientID,
+        //      'emergencyContactID' => $patient->emergencyContactID,
+        //      'admissionDate' => $patient->admissionDate,
+        //      'emailID' => $patient->emailID,
+        //      'familyCode' => $patient->familyCode,
+        //      'roleID' => $patient->roleID,
+        //      'patientGroups' => $patientGroups, // Include patient groups in the response
+        //  ]);
      }
  
      /**
@@ -58,19 +58,19 @@ class PatientControllerAPI extends Controller
          // Handle the form submission and update patient information
          // Ensure you validate and sanitize the input data
  
-         $patient = Patients::find($id);
+        //  $patient = Patients::find($id);
  
-         if (!$patient) {
-             return response()->json(['error' => 'Patient not found'], 404);
-         }
+        //  if (!$patient) {
+        //      return response()->json(['error' => 'Patient not found'], 404);
+        //  }
  
-         $patient->update([
-             'admissionDate' => $request->input('admissionDate'),
-             'emailID' => $request->input('emailID'),
-             // Update other fields as needed
-         ]);
+        //  $patient->update([
+        //      'admissionDate' => $request->input('admissionDate'),
+        //      'emailID' => $request->input('emailID'),
+        //      // Update other fields as needed
+        //  ]);
  
-         return response()->json(['message' => 'Patient information updated successfully']);
+        //  return response()->json(['message' => 'Patient information updated successfully']);
      }
 
     /**
